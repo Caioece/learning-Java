4020e4ed370ce75b4ab22ea922730dde60976bff
Caio Santos <caiosantosdev@gmail.com>
2023-07-24 23:19:24 -0300

    add arit ops

---
+++ Aritop.java
@@ -0,0 +1,11 @@
+public class Aritop{
+    public static void main(String[] args){
+        int resultado = 5 + 10;
+        System.out.println("O resultado da soma eh: " + resultado);
+        resultado = resultado * 5;
+        System.out.println("O resultado da mult eh: " + resultado);
+        resultado = resultado / 15;
+        System.out.println("O resultado da divisao eh: " + resultado);
+
+    }
+}
