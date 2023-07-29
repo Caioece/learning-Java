00debbefdbe2dc2226d1a184c57cc78f914a4277
Caio Santos <caiosantosdev@gmail.com>
2023-07-28 21:44:30 -0300

    att questao3 lista

---
+++ exercicios basic java Loiane Groner/aula-16-17/Questao3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Questao3
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String nome = scan.next();
+        
+    }
+}
