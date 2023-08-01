52f8063b13275801db44342e95face2d1ce8273b
caiosantosdev <caiosantosdev@gmail.com>
2023-08-01 12:41:42 -0300

    Jogo da velha pronto.

--- exercicios basic java Loiane Groner/aula-20/Jogodavelha.java
+++ exercicios basic java Loiane Groner/aula-20/Jogodavelha.java
@@ -13,8 +13,8 @@ public class Jogodavelha
             System.out.printf("=== JOGO DA VELHA ===%n");
             System.out.printf("JOGADOR 1 - O%n");
             System.out.printf("JOGADOR 2 - X%n");
-            System.out.printf("DIGITE '1' PARA JOGAR.");
-            System.out.printf("DIGITE '0' PARA SAIR.");
+            System.out.printf("DIGITE '1' PARA JOGAR.%n");
+            System.out.printf("DIGITE '0' PARA SAIR.%n");
             opcao = scan.nextInt();
             if(opcao == 1)
                 jogar = 1;
@@ -26,47 +26,58 @@ public class Jogodavelha
 
         int turn = 1;
         int ganhador = 0;
+        int rounds = 0;
+        int velha = 0;
         /*printando o jogo da velha*/
         while(jogar == 1 && jogando == 1)
         {
-            System.out.println("\t\t\t|    |"); /*primeira linha*/
+            System.out.printf("                 C0    C1    C2%n");
+            System.out.println("                    |    |"); /*primeira linha*/
             for(int i = 0 ; i < matrix.length ; i++)
             {
                 int firstTime = 1;
                 for(int j = 0 ; j < matrix.length ; j++)
                 {
-                    if(firstTime == 1 && matrix[i][j] == null)
-                    {
-                        System.out.printf("\t\t\t");
-                    }
-                    else if(firstTime == 1 && matrix[i][j] != null)
-                    {
-                        System.out.printf("\t\t     ");
-                    }
-                    if((matrix[i][j] == null && j < 2))
-                        System.out.printf("|    ");
-                    else if(matrix[i][j] != null && j< 2)
-                        System.out.printf("%s  | ", (matrix[i][j]));
-                    else if(matrix[i][j] != null && j == 2)
-                        System.out.printf("%s", matrix[i][j]);
+                    if(firstTime == 1)
+                        System.out.printf("          L%d    ", (i));
+                    if(matrix[i][j] == null && j < 2)
+                        System.out.printf("    |");
+                    if(matrix[i][j] != null && j < 2)
+                        System.out.printf(" %s  |", (matrix[i][j]));
+                    else if(matrix[i][j] != null & j==2)
+                        System.out.printf(" %s", (matrix[i][j]));
                     firstTime = 0;
-                    
                 }
                 if(i<2)
                 {
-                    System.out.printf("%n\t\t    ____|____|____%n"); /*linhas com underline*/
-                    System.out.printf("\t\t\t|    |%n");/*linha pos underline*/
+                    System.out.printf("%n                ____|____|____"); /*linhas com underline*/
+                    System.out.printf("%n                    |    |%n");/*linha pos underline*/
                 } 
             }
-            System.out.printf("%n\t\t\t|    |%n"); /*ultima linha*/
-            System.out.printf("JOGADOR %d - DIGITE A LINHA E COLUNA DE SUA JOGADA%n", (turn));
-            int linha = scan.nextInt();
-            int coluna = scan.nextInt();
+            System.out.printf("%n                    |    |%n"); /*ultima linha*/
+            int validTurn = 0;
+            int linha;
+            int coluna;
+            do{
+                System.out.printf("JOGADOR %d - DIGITE A LINHA E COLUNA DE SUA JOGADA%n", (turn));
+                linha = scan.nextInt();
+                coluna = scan.nextInt();
+                if(matrix[linha][coluna] != null)
+                    System.out.printf("JOGADA INVALIDA!%n");
+                else
+                    validTurn = 1;
+            }while(validTurn == 0);
             if(turn == 1)
+            {
                 matrix[linha][coluna] = "O";
+                rounds++;
+            }
             else
+            {
                 matrix[linha][coluna] = "X";
-                /*VERIFICANDO VENCEDOR*/
+                rounds++;
+            }
+                /*VERIFICANDO VENCEDOR OU VELHA*/
                 for(int i = 0 ; i < matrix.length ; i++)
                 {
                     /*verificando horizontal e vertical)*/
@@ -75,8 +86,15 @@ public class Jogodavelha
                     /*verificando diagonais*/
                     if( ( (matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2]) && matrix[0][0] != null) || ( (matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0]) && (matrix[0][2] != null) ) )
                         jogando = 0;
+                    
+                    if(jogando == 0)
+                        ganhador = turn;
+                    if(jogando == 1 && rounds == 9)
+                    {
+                        jogando = 0 ;
+                        velha = 1;
+                    }
 
-                    ganhador = turn;
                 }
             if(turn == 1)
                 turn = 2;
@@ -84,19 +102,49 @@ public class Jogodavelha
                 turn = 1;
         }
         if(jogar != 2)
-            System.out.printf("%nJOGADOR %d GANHOU !!", (ganhador));
+        {
+            System.out.printf("                 C0    C1    C2%n");
+            System.out.println("                    |    |"); /*primeira linha*/
+            for(int i = 0 ; i < matrix.length ; i++)
+            {
+                int firstTime = 1;
+                for(int j = 0 ; j < matrix.length ; j++)
+                {
+                    if(firstTime == 1)
+                        System.out.printf("          L%d    ", (i));
+                    if(matrix[i][j] == null && j < 2)
+                        System.out.printf("    |");
+                    if(matrix[i][j] != null && j < 2)
+                        System.out.printf(" %s  |", (matrix[i][j]));
+                    else if(matrix[i][j] != null & j==2)
+                        System.out.printf(" %s", (matrix[i][j]));
+                    firstTime = 0;
+                }
+                if(i<2)
+                {
+                    System.out.printf("%n                ____|____|____"); /*linhas com underline*/
+                    System.out.printf("%n                    |    |%n");/*linha pos underline*/
+                } 
+            }
+            System.out.printf("%n                    |    |%n"); /*ultima linha*/
+            if(velha != 1)
+                System.out.printf("%nJOGADOR %d GANHOU !!", (ganhador));
+            else
+                System.out.printf("%nDEU VELHA :( !!%n");
+        }
     }
 }
 
 
 /*
-            |    |
-            |    |    
-        ____|____|____
-            |    |
-            |    |   
-        ____|____|____
-            |    |
-            |    |    
-            |    |
+20 espacos
+                    |    |
+16 espacos fT       |    |    
+16 espacos      ____|____|____
+                    |    |
+                    |    |   
+                ____|____|____
+                    |    |
+                    |    |    
+                    |    |
 */
