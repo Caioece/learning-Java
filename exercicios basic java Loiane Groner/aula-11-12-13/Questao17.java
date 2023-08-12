0df0a6d86d8f6955a32b14dc36e03db056a14a22
Caio Santos <caiosantosdev@gmail.com>
2023-08-12 15:32:46 -0300

    new names

--- exercicios basic java Loiane Groner/aula-11-12-13/Questao17.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Questao17
-{
-    public static void main(String[] args)
-    {
-        
-    }
-}
--- exercicios basic java Loiane Groner/aula-19/Array.java
+++
@@ -1,7 +0,0 @@
-public class Array
-{
-    public static void main(String[] args)
-    {
-        double[] temperaturas = new double[365];
-    }
-}
